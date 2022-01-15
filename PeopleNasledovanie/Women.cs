ab01ea75e179a3b62485fdca199ee893b7dbe32c
SunchainNatali <95683549+SunchainNatali@users.noreply.github.com>
2022-01-15 18:55:29 +0200

    Update Women.cs

--- PeopleNasledovanie/Women.cs
+++ PeopleNasledovanie/Women.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+
 namespace PeopleNasledovanie
 {
     public abstract class Women:People
     {
         abstract public void Pregnancy(int numberOfTimes);
-        abstract public void GiveChild(int number);
-        
+        abstract public void GiveChild(int number);        
     }
 }
